39174af6efc91aaabf06615915f23a3623af2dc7
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2022-12-01 16:48:50 -0300

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,54 @@
+import classNames from "classnames";
+import React from "react";
+
+import HeadsetIcon from '../../assets/icon-headset.svg';
+import MailIcon from '../../assets/icon-mail.svg';
+import CorebizTag from '../../assets/corebiz-tag.svg';
+import VtexTag from '../../assets/vtex-tag.svg';
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer__container}>
+        <section className={styles.footer__location}>
+          <h3>Localização</h3>
+          <p>Avenida Andrômeda, 2000. Bloco 6 e 8</p>
+          <p>Alphavile SP</p>
+          <p>brasil@corebiz.ag</p>
+          <p>+55 11 3090 1039</p>
+        </section>
+
+        <div className={styles.footer__contact}>
+          <a href="#0" className={classNames({
+            [styles.footer__button]: true,
+            [styles.footer__button__mail]: true,
+          })}>
+            <MailIcon />
+            Entre em contato
+          </a>
+
+          <a href="#0" className={classNames({
+            [styles.footer__button]: true,
+            [styles.footer__button__phone]: true,
+          })}>
+            <HeadsetIcon />
+            Fale com o nosso consultor online
+          </a>
+        </div>
+
+        <div className={styles['footer__tags-block']}>
+          <div className={styles.footer__tag}>
+            <span>Created by</span>
+            <CorebizTag />
+          </div>
+
+          <div className={styles.footer__tag}>
+            <span>Powered by</span>
+            <VtexTag />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import 'normalize.css';
+import './index.css';
 
 const rootNode = document.getElementById('app')!;
 createRoot(rootNode).render(<App />);
